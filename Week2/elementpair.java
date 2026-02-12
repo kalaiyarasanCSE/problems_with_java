205a3f48c761c1595ce39369fb74c4ff6542df1a
kalaiyarasanCSE <vtu26102@veltech.edu.in>
2026-02-12 15:24:40 +0530

    Update elementpair.java

--- Week2/elementpair.java
+++ Week2/elementpair.java
@@ -1,1 +1,14 @@
 
+class elementpair{
+  public static void main(String[] arg){
+    int arr[]={1,2,3,4,5,6}
+      for(int i=0; i<arr.length; i++){
+      for(int j=i+1;j<arr.length;j++){
+        println("("+arr[i]+","+arr[j]+")");
+        
+      }
+    
+    }
+    
+  }
+}
