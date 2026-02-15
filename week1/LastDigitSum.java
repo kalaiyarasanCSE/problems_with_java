6cab6b13039be8a56d3743cde1caf1edc21425e2
kalaiyarasanCSE <vtu26102@veltech.edu.in>
2026-02-15 22:54:31 +0530

    Update LastDigitSum.java

--- week1/LastDigitSum.java
+++ week1/LastDigitSum.java
@@ -5,7 +5,6 @@ public class LastDigitSum {
 
         BiFunction<Integer, Integer, Integer> sumLastDigits =
                 (a, b) -> (a % 10) + (b % 10);
-
         System.out.println("Sum of last digits: " + sumLastDigits.apply(123, 456));
     }
 }
